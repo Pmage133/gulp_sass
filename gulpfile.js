aa702e46adaaef1d810349d5dca0b4ba6d7a7831
Pmage133 <39876683+Pmage133@users.noreply.github.com>
2020-04-14 15:40:39 +0300

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+﻿var gulp = require("gulp");
+var scss = require("gulp-sass");
+var browserSync = require('browser-sync');
+
+gulp.task("default", async function () {
+    console.log("This is default task...");
+}); 
+
+gulp.task('browser-sync', function (cb) {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+        notify:false
+    });
+    cb();
+});
+
+gulp.task("compile", function (cb) {
+    gulp.src("app/scss/**/*.scss").pipe(scss())
+        .pipe(gulp.dest("app/css")).pipe(browserSync.reload({ stream: true }));
+    cb();
+});
+
+gulp.task("watch", async function() {
+    await gulp.series('compile', 'browser-sync')();
+    console.log("--->");
+    gulp.watch("app/scss/**/*.scss", gulp.series("compile"));
+    //gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/*.html', async () => {
+        await browserSync.reload();
+        console.log("Reloaded!")
+    });
+});
---
+++ readme.txt
@@ -0,0 +1,15 @@
+cd папка_с_проектом
+
+npm init -f
+npm install gulp --save-dev
+npm install gulp-sass --save-dev
+npm install browser-sync --save-dev
+создать gulpfile.js и создать задачи (в т.ч. задачу watch)
+
+-----------
+npm install  (обновить зависимости)
+
+-----------
+
+gulp watch (запустить таску)
+(Ctrl+C когда нужно остановить дозор)
